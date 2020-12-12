5b35fbf39c8ce42651c5c69f0c8f2c947c0b4087
Aoda <hinokamiaoda@gmail.com>
2020-12-12 16:42:51 +0600

    fixed navbar link

--- src/Components/Navbar/NavbarAnimations.js
+++ src/Components/Navbar/NavbarAnimations.js
@@ -1,7 +1,4 @@
 import { gsap } from 'gsap'
-import { CSSRulePlugin } from 'gsap/all'
-
-gsap.registerPlugin(CSSRulePlugin)
 
 export const hideAniamtion = (onClick) => {
   const timeline = gsap.timeline({ onComplete: onClick })
@@ -9,13 +6,11 @@ export const hideAniamtion = (onClick) => {
 }
 
 export const borderMouseLeave = (className) => {
-  let rule = CSSRulePlugin.getRule(`.${className}::after`)
-  gsap.to(rule, { duration: 0.35, cssRule: { width: '0%', background: 'white' } })
+  document.querySelector(`.${className}`).classList.remove('added')
 }
 
 export const borderMouseMove = (className) => {
-  let rule = CSSRulePlugin.getRule(`.${className}::after`)
-  gsap.to(rule, { duration: 0.35, cssRule: { width: '80%', background: 'white' } })
+  document.querySelector(`.${className}`).classList.add('added')
 }
 
 export const brandAnim = (className) => {
